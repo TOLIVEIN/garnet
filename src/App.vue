c656cedaa51a5729ddabad1367736862d7cd6cb2
TOLIVEIN <TOLIVEIN@outlook.com>
2020-08-12 17:03:14 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h1>Garnet</h1>
+    <home-vue message="Vue 3 + TypeScript App"/>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-
+import HomeVue from './components/Home.vue';
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    HomeVue
   }
 });
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
